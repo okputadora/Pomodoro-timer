a6a8b152c620c432adf61ebe77a4dbdc8b5eb971
okputadora <okputadora@gmail.com>
2016-11-20 22:37:26 -0500

    First timer works

--- pom.js
+++ pom.js
@@ -3,6 +3,22 @@ var workTime = 25;
 var clockOn = true;
 var second = 59;
 
+function displaySecond(){
+  console.log("in here");
+  if (second > 9){
+    $("#timer").html(workTime + ":" + second);
+  }
+  else{
+    $("#timer").html(workTime + ":0" + second);
+  }
+  console.log(second);
+  if (second === 0){
+    workTime -= 1;
+    second = 60;
+  }
+  second -= 1;
+}
+
 // Add or subtract time from clocks
 $("#breakPlus").click(function(){
   breakTime +=  1;
@@ -26,13 +42,9 @@ $("#workMinus").click(function(){
 
 // Start clock
 $("#start").click(function(){
-  while (clockOn === true){
-    for (var i = workTime; i = 0; i--){
-      while(second >= 0){
-        setInterval(displaySecond, 1000);
-      }
-      second = 59;
+  $("#timer").html()
+  console.log("button working");
+      setInterval(displaySecond, 1000);
       workTime -= 1;
-    }
-  }
+
 });
