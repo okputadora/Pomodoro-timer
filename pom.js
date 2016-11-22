723758e6d54c2d3714c7704150c07f5b69e59efe
okputadora <okputadora@gmail.com>
2016-11-22 14:06:55 -0500

    Timer cycles between work and break Few bugs:
    1. after going back to work for the first time both setIntervals are called.
    2. something weird happens when it goes to zero (displays 1 in the seconds)

--- pom.js
+++ pom.js
@@ -1,56 +1,99 @@
-var breakTime = 5;
-var workTime = 25;
-var clockOn = true;
-var second = 60;
+var ogBreakTime = 1;
+var ogWorkTime = 1;
+var second = 0;
 var totalSeconds;
-var secToPer;
-var per = 100;
+var workSecToPer;
+var breakSecToPer;
+var percent = 100;
+// declare variable to stop setInterval here so all
+// functions have acces to it
+var stop;
 
-function displaySecond(){
-  second -= 1;
-  per -= secToPer;
-  $("#work-remaining").css("height", per + "%");
+function countdown(percentage, id){
+  console.log(percentage);
+  console.log(percent);
+  percent -= percentage;
+  $("#" + id + "-remaining").css("height", percent + "%");
   if (second > 9){
-    $("#timer").html(workTime + ":" + second);
+    $("#timer").html(time + ":" + second);
   }
   else{
-    $("#timer").html(workTime + ":0" + second);
+    $("#timer").html(time + ":0" + second);
   }
   console.log(second);
   if (second === 0){
-    workTime -= 1;
-    second = 60;
+    if (time > 0){
+      time -= 1;
+      second = 60;
+    }
+    console.log("minute: " + time);
   }
   second -= 1;
+  if (time === 0 && second === 0 && id === "work"){
+    console.log("Worktime done");
+    percent = 100;
+    clearInterval(stop);
+    id = "break";
+    time = ogWorkTime;
+    // change title to break
+    $("#sess-type").html("Break");
+    // reset graph
+    $("#work-remaining").css("height", "100%");
+    setInterval(function(){
+      stop = countdown(breakSecToPer, id);
+    }, 1000);
+  }
+  if (time === 0 && second === 0 && id === "break"){
+    console.log("breaktime done");
+    percent = 100;
+    clearInterval(stop);
+    id = "work";
+    second = 0;
+    time = ogBreakTime;
+    // change title to break
+    $("#sess-type").html("Work");
+    // reset graph
+    $("#break-remaining").css("height", "100%");
+    setInterval(function(){
+      stop = countdown(workSecToPer, id);
+    }, 1000);
+  }
 }
-
 // Add or subtract time from clocks
 $("#breakPlus").click(function(){
-  breakTime +=  1;
-  $("#breakTime").html(breakTime);
+  ogBreakTime +=  1;
+  $("#breakTime").html(ogBreakTime);
 });
 
 $("#workPlus").click(function(){
-  workTime += 1;
-  $("#workTime").html(workTime);
+  ogWorkTime += 1;
+  $("#workTime").html(ogWorkTime);
 });
 
 $("#breakMinus").click(function(){
-  breakTime -= 1;
-  $("#breakTime").html(breakTime);
+  ogBreakTime -= 1;
+  $("#breakTime").html(ogBreakTime);
 });
 
 $("#workMinus").click(function(){
-  workTime -= 1;
-  $("#workTime").html(workTime);
+  ogWorkTime -= 1;
+  $("#workTime").html(ogWorkTime);
 });
 
 // Start clock
 $("#start").click(function(){
+  breakTime = ogBreakTime;
+  workTime = ogWorkTime;
   $("#timer").html(workTime + ":00");
-  totalSeconds = workTime * 60;
-  secToPer = 100/totalSeconds;
-  console.log(secToPer);
-  console.log(totalSeconds)
-  setInterval(displaySecond, 1000);
+  totalSecondsWork = workTime * 60;
+  totalSecondsBreak = breakTime * 60;
+  workSecToPer = 100/totalSecondsWork;
+  breakSecToPer = 100/totalSecondsBreak;
+  console.log(breakSecToPer);
+  id = "work";
+  console.log("starting time " + workTime);
+  time = workTime;
+  stop = setInterval(function(){
+    countdown(workSecToPer, id);
+  }, 1000);
 });
