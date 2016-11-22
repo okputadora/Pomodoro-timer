92bbcac5e36e5390b735da9d09207fdfd6a7fd5e
okputadora <okputadora@gmail.com>
2016-11-22 14:58:33 -0500

    Condense code

--- pom.js
+++ pom.js
@@ -1,62 +1,56 @@
 var ogBreakTime = 1;
 var ogWorkTime = 1;
-var second = 0;
+var second = 59;
 var totalSeconds;
 var workSecToPer;
 var breakSecToPer;
 var percent = 100;
+var id;
 // declare variable to stop setInterval here so all
 // functions have acces to it
 var stop;
 
 function countdown(percentage, id){
-  console.log(percentage);
-  console.log(percent);
   percent -= percentage;
-  $("#" + id + "-remaining").css("height", percent + "%");
   if (second > 9){
     $("#timer").html(time + ":" + second);
   }
   else{
     $("#timer").html(time + ":0" + second);
   }
-  console.log(second);
   if (second === 0){
     if (time > 0){
       time -= 1;
       second = 60;
     }
-    console.log("minute: " + time);
   }
   second -= 1;
-  if (time === 0 && second === 0 && id === "work"){
-    console.log("Worktime done");
+  $("#" + id + "-remaining").css("height", percent + "%");
+  if (time === 0 && second === -1){
+    console.log("calling new function");
     percent = 100;
     clearInterval(stop);
-    id = "break";
-    time = ogWorkTime;
-    // change title to break
-    $("#sess-type").html("Break");
     // reset graph
-    $("#work-remaining").css("height", "100%");
-    stop = setInterval(function(){
-      countdown(breakSecToPer, id);
-    }, 1000);
-  }
-  if (time === 0 && second === 0 && id === "break"){
-    console.log("breaktime done");
-    percent = 100;
-    clearInterval(stop);
-    id = "work";
-    second = 0;
-    time = ogBreakTime;
-    // change title to break
-    $("#sess-type").html("Work");
-    // reset graph
-    $("#break-remaining").css("height", "100%");
-    stop = setInterval(function(){
-      countdown(workSecToPer, id);
-    }, 1000);
+    $("#" + id + "-remaining").css("height", "100%");
+    second = 59;
+    if (id === "Work"){
+      console.log("id = work");
+      time = ogBreakTime-1;
+      id = "Break";
+      stop = setInterval(function(){
+        countdown(breakSecToPer, id);
+      }, 1000);
+    }
+    else if (id === "Break"){
+      console.log("id = break");
+      time = ogWorkTime-1;
+      id = "Work";
+      stop = setInterval(function(){
+        countdown(workSecToPer, id);
+      }, 1000);
+    }
+    // toggle session type;
+    $("#sess-type").html(id);
   }
 }
 // Add or subtract time from clocks
@@ -90,9 +84,9 @@ $("#start").click(function(){
   workSecToPer = 100/totalSecondsWork;
   breakSecToPer = 100/totalSecondsBreak;
   console.log(breakSecToPer);
-  id = "work";
+  id = "Work";
   console.log("starting time " + workTime);
-  time = workTime;
+  time = workTime - 1;
   stop = setInterval(function(){
     countdown(workSecToPer, id);
   }, 1000);
